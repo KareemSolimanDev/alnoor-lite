cb093a9bef0552974c1fe29e2cbbcd5bb55aaad9
KareemAlsayd <kareemalsaydo@gmail.com>
2023-04-02 10:59:13 +0200

    add views and models

---
+++ models.py
@@ -0,0 +1,26 @@
+from run import db
+from datetime import datetime
+
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    fname=db.Column(db.String,nullable=False)
+    uname=db.Column(db.String,nullable=False,unique=True)
+    email=db.Column(db.String,nullable=False,unique=True)
+    password=db.Column(db.String,nullable=False)
+    role=db.Column(db.String(5),nullable=False,default='user')
+    lessons=db.relationship('Lesson',backref='author',lazy=True)
+    request=db.relationship('Request',backref='user',lazy=True)
+
+class Lesson(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    image=db.Column(db.String,nullable=False)
+    title=db.Column(db.String,nullable=False,unique=True)
+    content=db.Column(db.Text,nullable=False)
+    date_posted=db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
+
+
+class Request(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
+    time=db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
--- run.py
+++ run.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app=Flask()
+app=Flask(__name__)
 
 app.config['SECRET_KEY']='secret_key'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.db'
 
 db=SQLAlchemy(app)
 
+from models import *
+from routes import *
 
 if __name__=='__main__':
     app.run(debug=True)
