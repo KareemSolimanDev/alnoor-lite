1fb7e01f658b9680edbd7b0407b41ccae5229370
KareemAlsayd <kareemalsaydo@gmail.com>
2023-04-18 19:33:24 +0200

    handle admin page with flask-admin

--- run.py
+++ run.py
@@ -1,21 +1,35 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_ckeditor import CKEditor
-from flask_login import LoginManager
-
+from flask_login import LoginManager,current_user
+from flask_admin import Admin
+from flask_admin import AdminIndexView
+from flask_admin.contrib.sqla import ModelView
 app=Flask(__name__)
 
 app.config['SECRET_KEY']='secret_key'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.db'
+
+class MyAdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        return (current_user.role=='admin')
+
+class MyModelView(ModelView):
+    def is_accessible(self):
+        return (current_user.role=='admin')
 
 db=SQLAlchemy(app)
 ckeditor = CKEditor(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "info"
+admin = Admin(app, index_view=MyAdminIndexView())
 
 from routes import *
 from models import *
+
+admin.add_view(MyModelView(User, db.session))
+admin.add_view(MyModelView(Lesson, db.session))
 # =========================
 # deal with db
 # =========================
