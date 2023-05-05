fdf77c6d65b66f873eedfb3a9ddb82c7c9584565
KareemAlsayd <kareemalsaydo@gmail.com>
2023-05-05 13:59:27 +0300

    add buttons to user page (create,admin)

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from flask_login import LoginManager,current_user
 from flask_admin import Admin
 from flask_admin import AdminIndexView
 from flask_admin.contrib.sqla import ModelView
+from flask_babel import Babel
 app=Flask(__name__)
 
 app.config['SECRET_KEY']='secret_key'
@@ -25,6 +26,7 @@ login_manager.login_view = "login"
 login_manager.login_message_category = "info"
 login_manager.login_message='يجب تسجيل الدخول للوصول لهذه الصفحه'
 admin = Admin(app, index_view=MyAdminIndexView())
+babel=Babel(app)
 
 from routes import *
 from models import *
